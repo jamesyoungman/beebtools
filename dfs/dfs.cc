32bcf89e43ae3c82516f02398495c325948e8b1b
James Youngman <james@youngman.org>
2020-04-19 00:23:04 +0100

    [dfs] Initial implementation of *INFO.
    There seems to be a problem with the load and execution address
    formatting.  The Beeb seems to print some of these as sign
    extended (e.g. on the emulator I am using *INFO prints FF1900 for the
    file W.SKETCH on the Welcome disk, rather than the value 031900 which
    is what this version of the code prints).

--- dfs/dfs.cc
+++ dfs/dfs.cc
@@ -1,9 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <getopt.h>
+#include <limits.h>
 
 #include <algorithm>
+#include <map>
 #include <fstream>
+#include <functional>
 #include <iostream>
 #include <iomanip>
 #include <vector>
@@ -27,7 +31,8 @@ enum class Format
   {
    HDFS,
    DFS,
-   WDFS
+   WDFS,
+   Solidisk,
   };
 
 namespace
@@ -102,14 +107,24 @@ namespace
 class CatalogEntry
 {
 public:
-  CatalogEntry(vector<byte>::const_iterator image_data, int slot)
-    : data_(image_data), cat_offset_(slot * 8)
+  CatalogEntry(vector<byte>::const_iterator image_data, int slot, Format fmt)
+    : data_(image_data), cat_offset_(slot * 8), fmt_(fmt)
   {
   }
 
   CatalogEntry(const CatalogEntry& other)
-    : data_(other.data_), cat_offset_(other.cat_offset_)
+    : data_(other.data_), cat_offset_(other.cat_offset_), fmt_(other.fmt_)
   {
+  }
+
+  inline unsigned char getbyte(unsigned int sector, unsigned short record_off) const
+  {
+    return data_[sector * 0x100 + record_off + cat_offset_];
+  }
+
+  inline unsigned long getword(unsigned int sector, unsigned short record_off) const
+  {
+    return getbyte(sector, record_off) | (getbyte(sector, record_off + 1) << 8);
   }
 
   string name() const
@@ -127,9 +142,36 @@ public:
     return (1 << 7) & data_[cat_offset_ + 0x07];
   }
 
+  unsigned long load_address() const
+  {
+    unsigned long address = getword(1, 0x00);
+    address |= ((getbyte(1, 0x06) >> 2) & 3) << 16;
+    // On Solidisk there is apparently a second copy of bits 16 and 17
+    // of the load address, but we only need one copy.
+    return address;
+  }
+
+  unsigned long exec_address() const
+  {
+    return getword(1, 0x02)
+      | ((getbyte(1, 0x06) >> 6) & 3) << 16;
+  }
+
+  unsigned long file_length() const
+  {
+    return getword(1, 0x4)
+      | ((getbyte(1, 0x06) >> 4) & 3) << 16;
+  }
+
+  unsigned short start_sector() const
+  {
+    return getbyte(1, 0x07) | ((getbyte(1, 0x06) & 3) << 8);
+  }
+
 private:
   vector<byte>::const_iterator data_;
   offset cat_offset_;
+  Format fmt_;
 };
 
 
@@ -182,9 +224,8 @@ public:
   CatalogEntry get_catalog_entry(int index) const
   {
     // TODO: bounds checking
-    return CatalogEntry(img_.cbegin(), index);
+    return CatalogEntry(img_.cbegin(), index, disc_format());
   }
-
 
   sector_count_type disc_sector_count() const
   {
@@ -233,10 +274,27 @@ inline bool case_insensitive_less(const string& left,
   return tolower(*result.first) < tolower(*result.second);
 }
 
-inline
-bool dump_catalog(const Image& image)
+
+bool cmd_info(const Image& image, const char)
 {
-  const char current_directory = '$';
+  const int entries = image.catalog_entry_count();
+  cout << std::hex;
+  cout << std::uppercase;
+  for (int i = 1; i <= entries; ++i)
+    {
+      auto entry = image.get_catalog_entry(i);
+      cout << entry.directory() << "." << entry.name() << " "
+	   << (entry.is_locked() ? "L" : " ")
+	   << std::setw(6) << std::setfill('0') << entry.load_address() << " "
+	   << std::setw(6) << std::setfill('0') << entry.exec_address() << " "
+	   << std::setw(6) << std::setfill('0') << entry.file_length() << " "
+	   << std::setw(3) << std::setfill('0') << entry.start_sector() << "\n";
+    }
+  return true;
+}
+
+bool cmd_cat(const Image& image, const char current_directory)
+{
   cout << image.title();
   if (image.disc_format() != Format::HDFS)
     {
@@ -340,20 +398,85 @@ bool load_image(const char *filename, vector<byte>* image)
   return infile.read(reinterpret_cast<char*>(image->data()), len).good();
 }
 
+namespace
+{
+  typedef std::function<bool(const Image&, const char)> Command;
+  std::map<string, Command> commands;
+};
+
+bool cmd_help(const Image&, const char)
+{
+  cout << "Known commands:\n";
+  for (const auto& c : commands)
+      cout << "    " << c.first << "\n";
+  return true;
+}
+
+enum OptSignifier
+  {
+   OPT_IMAGE_FILE = SCHAR_MIN,
+   OPT_CWD,
+  };
+
 int main (int argc, char *argv[])
 {
-  for (int i = 1; i < argc; ++i)
+  const char *image_file = NULL;
+  char wd = '$';
+  int longindex;
+  static const struct option opts[] =
     {
-      vector<byte> image;
-      std::cout << "Dumping from " << argv[i] << "\n";
-      if (!load_image(argv[i], &image))
+     { "file", 1, NULL, OPT_IMAGE_FILE },
+     { "dir", 1, NULL, OPT_CWD },
+     { 0, 0, 0, 0 },
+    };
+
+  int opt;
+  while ((opt=getopt_long(argc, argv, "+", opts, &longindex)) != -1)
+    {
+      switch (opt)
 	{
-	  cerr << "failed to dump " << argv[i] << ": " << strerror(errno) << "\n";
+	case '?':  // Unknown option or missing option argument.
+	  // An error message was already issued.
 	  return 1;
+
+	case OPT_IMAGE_FILE:
+	  image_file = optarg;
+	  break;
+
+	case OPT_CWD:
+	  if (strlen(optarg) != 1)
+	    {
+	      cerr << "Argument to --" << opts[longindex].name
+		   << " should have one character only.\n";
+	      return 1;
+	    }
+	  wd = optarg[0];
+	  break;
 	}
-      dump_catalog(image);
     }
-  return 0;
+  if (optind == argc)
+    {
+      cerr << "Please specify a command (try \"help\")\n";
+      return 1;
+    }
+
+  commands = { {"cat", cmd_cat}, {"help", cmd_help}, { "info", cmd_info } };
+  const string cmd_name = argv[optind];
+  auto selected_command = commands.find(cmd_name);
+  if (selected_command == commands.end())
+    {
+      cerr << "unknown command " << cmd_name << "\n";
+      return 1;
+    }
+
+  vector<byte> image;
+  if (!load_image(image_file, &image))
+    {
+      cerr << "failed to dump " << image_file
+	   << ": " << strerror(errno) << "\n";
+      return 1;
+    }
+  return selected_command->second(image, wd) ? 1 : 0;
 }
 
 
