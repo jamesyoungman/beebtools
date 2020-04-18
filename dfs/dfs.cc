3accce38f0306b57bf43399721a30f04495cef93
James Youngman <james@youngman.org>
2020-04-18 20:40:40 +0100

    [dfs] Show the directory of files not in the current directory.

--- dfs/dfs.cc
+++ dfs/dfs.cc
@@ -23,18 +23,18 @@ typedef unsigned char byte;
 typedef vector<byte>::size_type offset;
 typedef unsigned short sector_count_type; // needs 10 bits
 
-enum class Format 
+enum class Format
   {
    HDFS,
    DFS,
    WDFS
   };
 
-namespace 
+namespace
 {
-  inline static string decode_opt(byte opt) 
+  inline static string decode_opt(byte opt)
   {
-    switch (opt) 
+    switch (opt)
       {
       case 0: return "off";
       case 1: return "load";
@@ -45,25 +45,25 @@ namespace
   }
 
 
-  inline char byte_to_char(byte b) 
+  inline char byte_to_char(byte b)
   {
     return char(b);
   }
 
-  inline char byte_to_ascii7(byte b) 
+  inline char byte_to_ascii7(byte b)
   {
     return char(b & 0x7F);
   }
 
   inline string extract_string(const vector<byte>& image,
-			       offset position, offset size) 
+			       offset position, offset size)
   {
     std::string result;
     std::transform(image.begin()+position, image.begin()+position+size,
 		   std::back_inserter(result), byte_to_char);
     return result;
   }
-  
+
   inline offset sector(int sector_num)
   {
     return SECTOR_BYTES * sector_num;
@@ -72,13 +72,13 @@ namespace
   inline Format identify_format(const vector<byte>& image)
   {
     const bool hdfs = image[0x106] & 8;
-    if (hdfs) 
+    if (hdfs)
       return Format::HDFS;
 
     // Look for the Watford DFS recognition string
     // in the initial entry in its extended catalog.
     if (std::all_of(image.begin()+0x200, image.begin()+0x208,
-		    [](byte b) { return b = 0xAA; })) 
+		    [](byte b) { return b = 0xAA; }))
       {
 	return Format::WDFS;
       }
@@ -87,38 +87,49 @@ namespace
   }
 
   inline string ascii7_string(vector<byte>::const_iterator begin,
-			      vector<byte>::size_type len) 
+			      vector<byte>::size_type len)
   {
     string result;
     result.reserve(len);
-    std::transform(begin, begin+len, std::back_inserter(result), byte_to_ascii7);
+    std::transform(begin, begin+len,
+		   std::back_inserter(result), byte_to_ascii7);
     return result;
   }
 }
 
+// Note, a CatalogEntry holds an iterator into the disc image data so
+// it must not outlive the image data.
 class CatalogEntry
 {
 public:
   CatalogEntry(vector<byte>::const_iterator image_data, int slot)
-   : name_(ascii7_string(image_data + slot * 0x08, 0x07)),
-     directory_(image_data[slot * 0x08 + 0x100])
+    : data_(image_data), cat_offset_(slot * 8)
   {
   }
-  
-  string name() const 
+
+  CatalogEntry(const CatalogEntry& other)
+    : data_(other.data_), cat_offset_(other.cat_offset_)
   {
-    return name_;
   }
 
-
-  char directory() const 
+  string name() const
   {
-    return directory_;
+    return ascii7_string(data_ + cat_offset_, 0x07);
+  }
+
+  char directory() const
+  {
+    return 0x7F & (data_[cat_offset_ + 0x07]);
+  }
+
+  bool is_locked() const
+  {
+    return (1 << 7) & data_[cat_offset_ + 0x07];
   }
 
 private:
-  string name_;
-  char directory_;
+  vector<byte>::const_iterator data_;
+  offset cat_offset_;
 };
 
 
@@ -129,8 +140,8 @@ public:
     : img_(disc_image), disc_format_(identify_format(disc_image))
   {
   }
-  
-  string title() const 
+
+  string title() const
   {
     vector<byte> title_data;
     title_data.resize(12);
@@ -146,10 +157,10 @@ public:
   {
     return (img_[0x106] >> 4) & 0x03;
   }
-  
+
   inline int cycle_count() const
   {
-    if (disc_format_ != Format::HDFS) 
+    if (disc_format_ != Format::HDFS)
       {
 	return int(img_[0x104]);
       }
@@ -158,12 +169,12 @@ public:
 
   inline Format disc_format() const { return disc_format_; }
 
-  offset end_of_catalog() const 
+  offset end_of_catalog() const
   {
     return img_[0x105];
   }
-  
-  int catalog_entry_count() const 
+
+  int catalog_entry_count() const
   {
     return end_of_catalog() / 8;
   }
@@ -173,13 +184,13 @@ public:
     // TODO: bounds checking
     return CatalogEntry(img_.cbegin(), index);
   }
-  
+
 
   sector_count_type disc_sector_count() const
   {
     sector_count_type result = img_[0x107];
     result |= (img_[0x106] & 3) << 8;
-    if (disc_format_ == Format::HDFS) 
+    if (disc_format_ == Format::HDFS)
       {
 	// http://mdfs.net/Docs/Comp/Disk/Format/DFS disagrees with
 	// the HDFS manual on this (the former states both that this
@@ -189,16 +200,16 @@ public:
       }
     return result;
   }
-  
+
 private:
   vector<byte> img_;
   Format disc_format_;
 };
 
 inline bool case_insensitive_less(const string& left,
-				  const string& right) 
+				  const string& right)
 {
-  auto comp = 
+  auto comp =
     [](const unsigned char lhs,
        const unsigned char rhs)
     {
@@ -207,14 +218,14 @@ inline bool case_insensitive_less(const string& left,
   const auto result = std::mismatch(left.cbegin(), left.cend(),
 				    right.cbegin(), right.cend(),
 				    comp);
-  if (result.second == right.cend()) 
+  if (result.second == right.cend())
     {
       // Mismatch because right is at end-of-string.  Therefore either
       // the strings are the same length or right is shorter.  So left
       // cannot be less.
       return false;
     }
-  if (result.first == left.cend()) 
+  if (result.first == left.cend())
     {
       // Equal, so left cannot be less.
       return false;
@@ -222,12 +233,12 @@ inline bool case_insensitive_less(const string& left,
   return tolower(*result.first) < tolower(*result.second);
 }
 
-
-inline 
-bool dump_catalog(const Image& image) 
+inline
+bool dump_catalog(const Image& image)
 {
+  const char current_directory = '$';
   cout << image.title();
-  if (image.disc_format() != Format::HDFS) 
+  if (image.disc_format() != Format::HDFS)
     {
       cout << " ("  << std::setbase(16) << image.cycle_count()
 	   << std::setbase(10) << ") FM";
@@ -236,7 +247,9 @@ bool dump_catalog(const Image& image)
   const auto opt = image.opt_value();
   cout << "Drive 0            Option "
        << opt << " (" << decode_opt(opt) << ")\n";
-  cout << "Dir. :0.$          Lib. :0.$\n\n";
+  cout << "Dir. :0." << current_directory
+       << "          "
+       << "Lib. :0.$\n\n";
 
   const int entries = image.catalog_entry_count();
   vector<int> ordered_catalog_index;
@@ -245,41 +258,77 @@ bool dump_catalog(const Image& image)
   for (int i = 1; i <= entries; ++i)
     ordered_catalog_index.push_back(i);
 
+
+  auto compare_entries =
+    [&image, &current_directory](int left, int right) -> bool
+    {
+      const auto& l = image.get_catalog_entry(left);
+      const auto& r = image.get_catalog_entry(right);
+      // Ensure that entries in the current directory sort
+      // first.
+      auto mapdir =
+	[&current_directory] (char dir) -> char {
+	  return dir == current_directory ? 0 : tolower(dir);
+	};
+
+      if (mapdir(l.directory()) < mapdir(r.directory()))
+	return true;
+      if (mapdir(r.directory()) < mapdir(l.directory()))
+	return false;
+      // Same directory, compare names.
+      return case_insensitive_less
+	(image.get_catalog_entry(left).name(),
+	 image.get_catalog_entry(right).name());
+    };
+
   std::sort(ordered_catalog_index.begin()+1,
 	    ordered_catalog_index.end(),
-	    [&image](int left, int right)  -> bool
-	    {
-	      return case_insensitive_less
-		(image.get_catalog_entry(left).name(), 
-		 image.get_catalog_entry(right).name());
-	    });
-	      
-  bool need_newline = false;
+	    compare_entries);
+
+  bool left_column = true;
+  bool printed_gap = false;
   for (int i = 1; i <= entries; ++i)
     {
       auto entry = image.get_catalog_entry(ordered_catalog_index[i]);
-      if (i & 1)
+      if (entry.directory() != current_directory)
 	{
-	  // We index from 1 so this is the left column.
-	  cout << "   " << entry.name();
-	  need_newline = true;
+	  if (!printed_gap)
+	    {
+	      if (i > 1)
+		cout << (left_column ? "\n" : "\n\n");
+	      left_column = true;
+	      printed_gap = true;
+	    }
 	}
+
+      if (!left_column)
+	{
+	  cout << std::setw(6) << "";
+	}
+
+      cout << " ";
+      if (entry.directory() != current_directory)
+	cout << " " << entry.directory() << ".";
       else
+	cout << "   ";
+      cout << entry.name();
+      if (entry.is_locked())
+	  cout << " L";
+      else
+	  cout << "  ";
+
+      if (!left_column)
 	{
-	  cout << std::setw(19) << entry.name();
 	  cout << "\n";
-	  need_newline = false;
 	}
+      left_column = !left_column;
     }
-  if (need_newline) 
-    {
-      cout << "\n";
-    }
+  cout << "\n";
   return true;
 }
 
 
-bool load_image(const char *filename, vector<byte>* image) 
+bool load_image(const char *filename, vector<byte>* image)
 {
   std::ifstream infile(filename, std::ifstream::in);
   if (!infile.seekg(0, infile.end))
@@ -291,9 +340,9 @@ bool load_image(const char *filename, vector<byte>* image)
   return infile.read(reinterpret_cast<char*>(image->data()), len).good();
 }
 
-int main (int argc, char *argv[]) 
+int main (int argc, char *argv[])
 {
-  for (int i = 1; i < argc; ++i) 
+  for (int i = 1; i < argc; ++i)
     {
       vector<byte> image;
       std::cout << "Dumping from " << argv[i] << "\n";
