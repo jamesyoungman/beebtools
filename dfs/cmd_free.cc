e3a13c8cfd997d0c668c719d235c655e7c31260b
James Youngman <james@youngman.org>
2020-05-10 12:12:41 +0100

    [dfs] Move implementation of free into a separate file.

---
+++ dfs/cmd_free.cc
@@ -0,0 +1,104 @@
+#include "commands.h"
+
+#include <iomanip>
+#include <iostream>
+#include <locale>
+#include <string>
+
+#include "dfsimage.h"
+	     
+namespace 
+{
+struct comma_thousands : std::numpunct<char>
+{
+  char do_thousands_sep() const
+  {
+    return ',';
+  }
+
+  std::string do_grouping() const
+  {
+    return "\3";		// groups of 3 digits
+  }
+};
+
+class CommandFree : public DFS::CommandInterface // *FREE
+{
+public:
+  const std::string name() const override
+  {
+    return "free";
+  }
+
+  const std::string usage() const override
+  {
+    return name() + " [drive]\n"
+      "The used/free space shown reflects the position of the last file on the disc.\n"
+      "Using *COMPACT or a similar tool on the disc may free up additional space.\n";
+  }
+
+  const std::string description() const override
+  {
+    return "display information about a disc's free space";
+  }
+
+  bool operator()(const DFS::StorageConfiguration& storage,
+		  const DFS::DFSContext& ctx,
+		  const std::vector<std::string>& args) override
+  {
+    const DFS::FileSystemImage *image;
+    if (args.size() > 2)
+      {
+	std::cerr << "at most one command-line argument is needed.\n";
+	return false;
+      }
+    if (args.size() < 2)
+      {
+	if (!storage.select_drive(ctx.current_drive, &image))
+	  return false;
+      }
+    else
+      {
+	if (!storage.select_drive_by_number(args[1], &image))
+	  return false;
+      }
+    assert(image != 0);
+
+    int sectors_used = 2;
+    const int entries = image->catalog_entry_count();
+    for (int i = 1; i <= entries; ++i)
+      {
+	const auto& entry = image->get_catalog_entry(i);
+	ldiv_t division = ldiv(entry.file_length(), DFS::SECTOR_BYTES);
+	const int sectors_for_this_file = division.quot + (division.rem ? 1 : 0);
+	const int last_sector_of_file = entry.start_sector() + sectors_for_this_file;
+	if (last_sector_of_file > sectors_used)
+	  {
+	    sectors_used = last_sector_of_file;
+	  }
+      }
+    int files_free = image->max_file_count() - image->catalog_entry_count();
+    int sectors_free = image->disc_sector_count() - sectors_used;
+    std::cout << std::uppercase;
+    auto show = [](int files, int sectors, const std::string& desc)
+		{
+		  std::cout << std::setw(2) << std::dec
+			    << files << " Files "
+			    << std::setw(3) << std::hex
+			    << sectors << " Sectors "
+			    << std::setw(7) << std::dec
+			    << (sectors * DFS::SECTOR_BYTES)
+			    << " Bytes " << desc << "\n";
+		};
+
+    auto prevlocale = std::cout.imbue(std::locale(std::cout.getloc(),
+					     new comma_thousands)); // takes ownership
+    show(files_free, sectors_free, "Free");
+    show(image->catalog_entry_count(), sectors_used, "Used");
+    std::cout.imbue(prevlocale);
+    return true;
+  }
+};
+REGISTER_COMMAND(CommandFree);
+
+} // namespace
