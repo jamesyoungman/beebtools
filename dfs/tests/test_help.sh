c80d55900174174f6d9d625dd490b9ce890a87fc
James Youngman <james@youngman.org>
2020-05-22 23:56:40 +0100

    [dfs] Improve test coverage of cmd_help.cc.

--- dfs/tests/test_help.sh
+++ dfs/tests/test_help.sh
@@ -13,20 +13,28 @@ if ! t=$(mktemp); then
 fi
 (
     rv=0
-    "${DFS}" help >| "${t}"
     commands="cat dump extract-files extract-unused free help info list sector_map space type"
-    for c in $commands
-    do
-	pat="^ *${c} *:"
-	if egrep -q "${pat}" < "${t}"
-	then
-	    echo "help output includes command ${c}, good"
-	else
-	    echo "help output does not include command ${c} (that is, does not match ${pat}):" >&2
-	    cat "${t}" >&2
-	    rv=1
-	fi
-    done
+
+    check() {
+	for c in $commands
+	do
+	    pat="^ *${c} *:"
+	    if egrep -q "${pat}" < "${t}"
+	    then
+		echo "help output includes command ${c}, good"
+	    else
+		echo "help output does not include command ${c} (that is, does not match ${pat}):" >&2
+		cat "${t}" >&2
+		rv=1
+	    fi
+	done
+    }
+
+    "${DFS}" help >| "${t}"
+    check
+    "${DFS}" help $commands >| "${t}"
+    check
+
 
     cmd_re='^ *[a-z][-_a-z]* *:'
     cmd_count=$(${DFS} help | grep -v 'usage:' | egrep -c -e "${cmd_re}")
@@ -37,6 +45,15 @@ fi
 	${DFS} help | grep -v 'usage:' | egrep -e "${cmd_re}" | nl -ba
 	rv=1
     fi
+
+    # Verify that we diagnose a problem for a nonexistent command.
+    nope="this-command-does-not-exist"
+    if "${DFS}" help "${nope}"
+    then
+	echo "Failed to diagnose problem with help ${nope}" >&2
+	rv=1
+    fi
+
     exit $rv
 )
 rv=$?
