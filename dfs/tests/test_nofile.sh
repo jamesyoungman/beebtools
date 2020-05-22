3103709a715ba5b414b1889df18103b3355b75f3
James Youngman <james@youngman.org>
2020-05-22 20:03:47 +0100

    [dfs] Added test; must diagnose a missing image file.

---
+++ dfs/tests/test_nofile.sh
@@ -0,0 +1,19 @@
+#! /bin/sh
+
+DFS="$1"
+shift
+TEST_DATA_DIR="$1"
+shift
+
+(
+    if "${DFS}" --file does_not_exist.ssd cat
+    then
+	echo 'expected non-zero return value for nonexistent file, got zero' >&2
+	exit 1
+    else
+	exit 0
+    fi
+)
+rv=$?
+cleanup
+( exit $rv )
