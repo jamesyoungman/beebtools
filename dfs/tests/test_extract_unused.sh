b9aca03450d8e0d2cb3841e202227b15a1cf85a5
James Youngman <james@youngman.org>
2020-05-24 08:25:52 +0100

    [dfs] Add negative cases for extract-unused.

--- dfs/tests/test_extract_unused.sh
+++ dfs/tests/test_extract_unused.sh
@@ -17,6 +17,7 @@ check_extract_unused() {
     fi
 
     cleanup() {
+	chmod +w "${d}"
 	rm -f "${d}"/*
 	rmdir "${d}"
     }
@@ -29,10 +30,31 @@ check_extract_unused() {
     gunzip < "${TEST_DATA_DIR}/${input}.gz" > "${input}" || exit 1
 
     (
+	# Positive case
 	if ! dfs extract-unused "${d}"; then
 	    echo "extract-unused command failed" >&2
 	    exit 1
 	fi
+
+	# Negative case: unwritable output directory.
+	rm -f "${d}"/*
+	chmod a-w "${d}"
+	if dfs extract-unused "${d}"; then
+	    echo "extract-unused command did not diagnose unwritable output directory" >&2
+	    ls -l "${d}" >&2
+	    exit 1
+	fi
+
+	# Invalid: request drive 1, no media in drive 1
+	chmod a+w "${d}"
+	rm -f "${d}"/*
+	if dfs extract-unused --drive 1 "${d}"
+	then
+	    echo "FAIL: extract-unused command doesn't diagnose missing media" >&2
+	    ls -l "${d}" >&2
+	    exit 1
+	fi
+
     )
     rv=$?
     cleanup
@@ -40,4 +62,20 @@ check_extract_unused() {
 }
 
 
-check_extract_unused acorn-dfs-sd-40t.ssd
+check_extract_unused acorn-dfs-sd-40t.ssd || exit 1
+
+# Now check some usage errors.
+
+# Invalid: no destination directory.
+if "${DFS}" --file "${TEST_DATA_DIR}/acorn-dfs-sd-40t.ssd.gz" extract-unused
+then
+    echo "FAIL: extract-unused command doesn't diagnose missing output directory" >&2
+    exit 1
+fi
+
+# Invalid: spurious extra arguments
+if "${DFS}" --file "${TEST_DATA_DIR}/acorn-dfs-sd-40t.ssd.gz" extract-unused outdir too-many-args
+then
+    echo "FAIL: extract-unused command doesn't diagnose spurious arguments" >&2
+    exit 1
+fi
