87f6665557fa03a4f4cb2b901e2f1171f2e122c5
James Youngman <james@youngman.org>
2020-06-27 09:41:07 +0100

    [basic] clean up temporary file in invariant_token_map_test.sh.

--- basic/invariant_token_map_test.sh
+++ basic/invariant_token_map_test.sh
@@ -3,15 +3,24 @@
 # Args: 1: bbcbasic_to_text executable
 #       2: golden output file
 
+# Ensure TMPDIR is set.
+: ${TMPDIR:=/tmp}
+
 set -e
-if ! tf=$(mktemp)
+if ! tf="$(mktemp  --tmpdir=${TMPDIR} invariant_token_map_test_XXXXXX)"
 then
     echo "Failed to create a temporary file" >&2
     exit 1
 fi
+
+cleanup() {
+    rm -f "${tf}"
+}
+
 if ! "$1" -D "${tf}"
 then
     echo "Failed to write the current token map" >&2
+    cleanup
     exit 1
 fi
 echo "Golden token file is $2"
@@ -19,6 +28,9 @@ echo "Current token file is ${tf}"
 if ! diff "$2" "${tf}"
 then
     echo "current token map does not match golden token map" >&2
+    cleanup
     exit 1
 fi
+
+cleanup
 exit 0
