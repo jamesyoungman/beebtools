411eef4e08958686e984fa722c526d6b5820d819
James Youngman <james@youngman.org>
2020-07-16 21:15:50 +0100

    Added a copyright header to source files.

--- dfs/driveselector.h
+++ dfs/driveselector.h
@@ -1,3 +1,18 @@
+//
+//   Copyright 2020 James Youngman
+//
+//   Licensed under the Apache License, Version 2.0 (the "License");
+//   you may not use this file except in compliance with the License.
+//   You may obtain a copy of the License at
+//
+//       http://www.apache.org/licenses/LICENSE-2.0
+//
+//   Unless required by applicable law or agreed to in writing, software
+//   distributed under the License is distributed on an "AS IS" BASIS,
+//   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//   See the License for the specific language governing permissions and
+//   limitations under the License.
+//
 // We draw a distinction between the identifier of a single surface of
 // a floppy disc and the identification of the immediate container of
 // a root catalog.  While in Acorn DFS these are the same thing, this
