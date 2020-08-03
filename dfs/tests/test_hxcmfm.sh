d55f4d4861c212c5c74e235700ee08de19a91620
James Youngman <james@youngman.org>
2020-08-03 10:21:22 +0100

    [dfs] Add tests for MFM floppy images in HFEv3 and HxC MFM formats.

---
+++ dfs/tests/test_hxcmfm.sh
@@ -0,0 +1,45 @@
+#! /bin/sh
+#
+#   Copyright 2020 James Youngman
+#
+#   Licensed under the Apache License, Version 2.0 (the "License");
+#   you may not use this file except in compliance with the License.
+#   You may obtain a copy of the License at
+#
+#       http://www.apache.org/licenses/LICENSE-2.0
+#
+#   Unless required by applicable law or agreed to in writing, software
+#   distributed under the License is distributed on an "AS IS" BASIS,
+#   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#   See the License for the specific language governing permissions and
+#   limitations under the License.
+#
+set -u
+# Tags: positive
+# Args:
+# ${DFS}" "${TEST_DATA_DIR}"
+DFS="$1"
+shift
+TEST_DATA_DIR="$1"
+shift
+
+# Ensure TMPDIR is set.
+: ${TMPDIR:?}
+
+input='wdfs-dd_HXCMFM_whatis.mfm.gz'
+
+expect_got() {
+    label="$1"
+    shift
+    if test "$1" != "$2"
+    then
+	printf 'test %s: expected:\n%s\ngot:\n%s\n' "${label}" "$1" "$2"
+	exit 1
+    fi
+}
+
+dfs() {
+    "${DFS}" --file "${TEST_DATA_DIR}/${input}" "$@"
+}
+
+expect_got WHATIS   "$(printf 'HXC MFM IMAGE CONTAINING A WDFS 62-FILE FILE SYSTEM\n')" "$(dfs type 'WHATIS' || echo __FAILED__)"
